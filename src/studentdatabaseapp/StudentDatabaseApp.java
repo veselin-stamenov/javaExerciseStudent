ac5f8e0c46848013a6286383b2573caede3a6173
veselin-stamenov <vesko983@gmail.com>
2017-12-06 00:06:23 +0200

    Student app

---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,8 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args){
+        Student student1 = new Student();
+        student1.enroll();
+    }
+}
